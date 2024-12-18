cf131874f304d557ffa1f6d77f3b0df5767597af
mattp-liberty <144935276+mattp-liberty@users.noreply.github.com>
2024-12-18 14:42:28 +0000

    Added DynamoDB table 'users'

--- main.tf
+++ main.tf
@@ -33,8 +33,31 @@ resource "aws_s3_bucket_public_access_block" "public_access" {
   restrict_public_buckets = true
 }
 
+# DynamoDB Table
+resource "aws_dynamodb_table" "users" {
+  name           = "users"
+  billing_mode   = "PAY_PER_REQUEST"  # On-demand pricing
+  hash_key       = "user_id"
+  
+  attribute {
+    name = "user_id"
+    type = "S"  # String type
+  }
+
+  tags = {
+    Environment = "production"
+    Project     = "PlayerAppAssessment"
+  }
+}
+
 # Output
 output "bucket_name" {
   description = "Name of the created S3 bucket"
   value       = aws_s3_bucket.main.id
+}
+
+# Additional output for DynamoDB
+output "dynamodb_table_name" {
+  description = "Name of the created DynamoDB table"
+  value       = aws_dynamodb_table.users.name
 }
