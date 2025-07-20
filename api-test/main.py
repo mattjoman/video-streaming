b05b9d3118adcefd286c83b08855cbe7007ae81c
mattp-liberty <144935276+mattp-liberty@users.noreply.github.com>
2025-07-20 18:51:45 +0100

    Added test

--- api-test/main.py
+++ api-test/main.py
@@ -1,10 +1,10 @@
 import requests
 import json
 
-def generate_mission(name: str, n: int, r: int, location: list[float], tags: list[str], cptSource: str="database"):
+def generate_mission(n: int, r: int, location: list[float], tags: list[str], cptSource: str="database"):
     url = "http://localhost:3000/checkpoints/generate-mission"
     body = {
-        "name": name,
+        "name": "",
         "n": n,
         "r": r,
         "location": {
@@ -19,9 +19,13 @@ def generate_mission(name: str, n: int, r: int, location: list[float], tags: lis
 
 
 def generate_and_save_mission():
-    mission = generate_mission(name="test", n=2, r=1, location=[-0.086, 51.518], tags=[])
+    mission = generate_mission(n=2, r=1, location=[-0.086, 51.518], tags=[])
     mission["config"]["name"] = 'testName'
-    print(json.dumps(mission, indent=4))
+
+    url = "http://localhost:3000/missions"
+    response = requests.post(url, json=mission)
+    print(response.json())
+
 
 if __name__ == "__main__":
     generate_and_save_mission()
