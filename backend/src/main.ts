783a4bf9c61f4a3a3a352678b9b72d4d1341e6c0
mattp-liberty <144935276+mattp-liberty@users.noreply.github.com>
2025-07-20 02:11:40 +0100

    Comment

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -37,7 +37,7 @@ export const handler = async (event: any, context: any) => {
   }
 };
 
-// Local development
+// Local development only
 if (process.env.NODE_ENV !== 'production') {
   bootstrap().then(async (app) => {
     await app.listen(3000);
