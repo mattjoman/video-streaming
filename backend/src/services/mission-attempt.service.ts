5894d858e6fc770b5bfbe9876a86675750c6bcdb
mattp-liberty <144935276+mattp-liberty@users.noreply.github.com>
2025-09-14 17:35:34 +0100

    Added _missionId

--- backend/src/services/mission-attempt.service.ts
+++ backend/src/services/mission-attempt.service.ts
@@ -6,14 +6,16 @@ import { AttemptCheckpointDto } from '../dto/checkpoint.dto';
 @Injectable()
 export class MissionAttemptService {
 
-  async startMissionAttempt(mission: MissionDto): Promise<MissionAttemptDto> {
+  async startMissionAttempt(mission: Partial<MissionDto>): Promise<MissionAttemptDto> {
     try {
       let missionAttempt = new MissionAttemptDto();
+      missionAttempt._missionId = mission?._id;
       missionAttempt.isCompleted = 0;
       missionAttempt.config = mission.config;
       let checkpoints: AttemptCheckpointDto[] = [];
       for (let checkpoint of mission.checkpoints) {
         let attemptCheckpoint = new AttemptCheckpointDto();
+        attemptCheckpoint._id = checkpoint._id;
         attemptCheckpoint.isVisited = 0;
         attemptCheckpoint.name = checkpoint.name;
         attemptCheckpoint.location = checkpoint.location;
