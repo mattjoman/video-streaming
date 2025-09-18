d213790f98984c75cacf0f0958c7ea5c60cf1296
mattp-liberty <144935276+mattp-liberty@users.noreply.github.com>
2025-09-18 21:47:30 +0100

    Display mission attempt on the map

--- frontend/src/components/Maps/AttemptMissionMap.tsx
+++ frontend/src/components/Maps/AttemptMissionMap.tsx
@@ -1,14 +1,22 @@
 import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import { Map, AdvancedMarker } from '@vis.gl/react-google-maps';
+import { AttemptCheckpoint } from '../../types';
 
 function AttemptMissionMap() {
+  const dispatch = useDispatch();
+
+  const missionAttempt = useSelector((state: any) => state.missionAttempt);
+
   return (
     <Map reuseMaps={true} mapId="map" colorScheme="LIGHT" renderingType="RASTER"
       defaultCenter={{ lat: 0, lng: 0 }}
       defaultZoom={5}
-      style={{ width: '100vh', height: '100vh' }}
+      style={{ width: '100vh', height: '50vh' }}
     >
-      <AdvancedMarker position={{ lat: 51, lng: 0 }} />
+      {missionAttempt?.checkpoints && missionAttempt.checkpoints.map((checkpoint: AttemptCheckpoint, idx: number) => (
+        <AdvancedMarker key={idx} position={{ lng: checkpoint.location.coordinates[0], lat: checkpoint.location.coordinates[1] }} />
+      ))}
     </Map>
   )
 }
