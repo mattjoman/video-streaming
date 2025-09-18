3a06974a30bb73a1731dd8620264e2308145ddbc
mattp-liberty <144935276+mattp-liberty@users.noreply.github.com>
2025-09-18 15:52:53 +0100

    Click on map to set mission config location

--- frontend/src/components/Maps/CreateMissionMap.tsx
+++ frontend/src/components/Maps/CreateMissionMap.tsx
@@ -1,18 +1,28 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { Map, AdvancedMarker } from '@vis.gl/react-google-maps';
+import { Checkpoint } from '../../types';
+import { updateCreateMissionConfig } from '../../store/slices/createMissionConfigSlice';
 
 function CreateMissionMap() {
+  const dispatch = useDispatch();
+
   const createMissionConfig = useSelector((state: any) => state.createMissionConfig);
   const createdMission = useSelector((state: any) => state.createdMission);
+
+  const handleMapClick = (e: any) => {
+    dispatch(updateCreateMissionConfig({ location: { type: 'Point', coordinates: [e.detail.latLng.lng, e.detail.latLng.lat] } }));
+  }
+
   return (
     <Map reuseMaps={true} mapId="map" colorScheme="LIGHT" renderingType="RASTER"
       defaultCenter={{ lat: 0, lng: 0 }}
       defaultZoom={5}
-      style={{ width: '100vh', height: '100vh' }}
+      style={{ width: '100vh', height: '50vh' }}
+      onClick={handleMapClick}
     >
-      <AdvancedMarker position={{ lat: 0, lng: 0 }} />
-      {createdMission?.checkpoints && createdMission.checkpoints.map((checkpoint: any, idx: number) => (
+      <AdvancedMarker position={{ lng: createMissionConfig.location.coordinates[0], lat: createMissionConfig.location.coordinates[1] }} />
+      {createdMission?.checkpoints && createdMission.checkpoints.map((checkpoint: Checkpoint, idx: number) => (
         <AdvancedMarker key={idx} position={{ lat: checkpoint.location.coordinates[1], lng: checkpoint.location.coordinates[0] }} />
       ))}
     </Map>
