85cce927ff22080e2be9d7ae627eb2162ff8b760
mattp-liberty <144935276+mattp-liberty@users.noreply.github.com>
2025-10-05 17:47:27 +0100

    Can now visit checkpoints

--- frontend/src/components/Maps/AttemptMissionMap.tsx
+++ frontend/src/components/Maps/AttemptMissionMap.tsx
@@ -1,24 +1,44 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import { useLocation, useLocationTest } from '../../hooks';
 import { Map } from '@vis.gl/react-google-maps';
 import { AttemptCheckpoint } from '../../types';
 import { CheckpointMarker } from './CheckpointMarker';
+import { VisitedCheckpointMarker } from './VisitedCheckpointMarker';
 import { UserLocationMarker } from './UserLocationMarker';
+import { isAtCheckpoint } from '../../helpers';
+import { visitCheckpoint } from '../../store/slices/missionAttemptSlice';
 
 function AttemptMissionMap() {
+  const dispatch = useDispatch();
 
   const missionAttempt = useSelector((state: any) => state.missionAttempt);
   const location = useLocationTest();
 
+  useEffect(() => {
+    //for (const checkpoint of missionAttempt.checkpoints) {
+    for (let idx = 0; idx < missionAttempt.checkpoints.length; idx++) {
+      const checkpoint = missionAttempt.checkpoints[idx];
+      if (isAtCheckpoint(location, {lng: checkpoint.location.coordinates[0], lat: checkpoint.location.coordinates[1]})) {
+        dispatch(visitCheckpoint({ idx }));
+      }
+    }
+  }, [location, missionAttempt.checkpoints]);
+
   return (
     <Map reuseMaps={true} mapId="map" colorScheme="LIGHT" renderingType="RASTER"
-      defaultCenter={{ lat: 0, lng: 0 }}
+      defaultCenter={{ lat: 51.25, lng: 0 }}
       defaultZoom={5}
       style={{ width: '100vh', height: '50vh' }}
     >
       {missionAttempt?.checkpoints && missionAttempt.checkpoints.map((checkpoint: AttemptCheckpoint, idx: number) => (
-        <CheckpointMarker key={idx} position={{ lng: checkpoint.location.coordinates[0], lat: checkpoint.location.coordinates[1] }} selectCheckpoint={() => null} />
+        <>
+          {checkpoint.isVisited ? (
+            <VisitedCheckpointMarker key={idx} position={{ lng: checkpoint.location.coordinates[0], lat: checkpoint.location.coordinates[1] }} />
+          ) : (
+            <CheckpointMarker key={idx} position={{ lng: checkpoint.location.coordinates[0], lat: checkpoint.location.coordinates[1] }} selectCheckpoint={() => null} />
+          )}
+        </>
       ))}
       <UserLocationMarker position={location} />
     </Map>
