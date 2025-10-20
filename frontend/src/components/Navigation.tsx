66fac9ee6b2fc0e079e4af21ffae629717a87e76
mattp-liberty <144935276+mattp-liberty@users.noreply.github.com>
2025-10-20 10:02:03 +0100

    Navigation layout/style

--- frontend/src/components/Navigation.tsx
+++ frontend/src/components/Navigation.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { pushPage, popPage } from '../store/slices/pageHistorySlice';
 import { Home, CreateMission, AttemptMission, SearchMissions, CreateCheckpoint, CreateManualMission } from './pages';
@@ -15,26 +15,54 @@ const pages = [
 export default function Navigation() {
   const dispatch = useDispatch();
   const history = useSelector((state: any) => state.pageHistory.history);
+  const [showNavigation, setShowNavigation] = useState(false);
 
   const handleNavigate = (page: string) => {
     dispatch(pushPage(page));
+    setShowNavigation(false);
   };
 
   return (
-    <div>
-      <div>
-        <button onClick={() => dispatch(popPage())}>Back</button>
-        {pages.map((page: { id: string, display: string }, index: number) => (
-          <button
-            key={index}
-            onClick={() => handleNavigate(page.id)}
-            style={history[history.length - 1] === page.id ? { backgroundColor: 'gray' } : { backgroundColor: 'white' }}
-          >
-            {page.display}
-          </button>
-        ))}
+    <div style={{
+      position: 'relative',
+      height: '100vh',
+    }}>
+      <div style={{ height: '3em', backgroundColor: '#aaffff' }}>
+        <button onClick={() => setShowNavigation(!showNavigation)}>Navigation</button>
       </div>
-      <div>
+
+      {showNavigation && (
+        <div style={{
+          height: 'calc(100vh - 3em)',
+          width: '100%',
+          position: 'absolute',
+          top: '3em',
+          zIndex: 1000,
+          backgroundColor: 'rgba(255, 255, 255, 0.5)',
+          backdropFilter: 'blur(10px)',
+          WebkitBackdropFilter: 'blur(10px)',
+        }}>
+          <button onClick={() => dispatch(popPage())}>Back</button>
+          {pages.map((page: { id: string, display: string }, index: number) => (
+            <button
+              key={index}
+              onClick={() => handleNavigate(page.id)}
+              style={history[history.length - 1] === page.id ? { backgroundColor: 'gray' } : { backgroundColor: 'white' }}
+            >
+              {page.display}
+            </button>
+          ))}
+        </div>
+      )}
+
+      <div style={{
+        height: 'calc(100vh - 3em)',
+        width: '100%',
+        overflow: 'auto',
+        backgroundColor: '#ffffaa',
+        position: 'absolute',
+        top: '3em',
+      }}>
         {history[history.length - 1] === 'home' ? (<Home />) : (<></>)}
         {history[history.length - 1] === 'create-mission' ? (<CreateMission />) : (<></>)}
         {history[history.length - 1] === 'create-manual-mission' ? (<CreateManualMission />) : (<></>)}
@@ -42,6 +70,7 @@ export default function Navigation() {
         {history[history.length - 1] === 'search-missions' ? (<SearchMissions />) : (<></>)}
         {history[history.length - 1] === 'create-checkpoint' ? (<CreateCheckpoint />) : (<></>)}
       </div>
+
     </div>
   );
 }
